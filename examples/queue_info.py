85d707b712b788279ba8643554866d07ccd26c68
PlatformLSF <zqzhai@cn.ibm.com>
2014-07-25 14:09:22 +0800

    Create queue_info.py

---
+++ examples/queue_info.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+from pythonlsf import lsf
+
+def query_queue(queue_name):
+    """
+    "query queue info"
+    """
+
+    if lsf.lsb_init("test") > 0:
+        return -1;
+
+    intp_num_queues = lsf.new_intp();
+    lsf.intp_assign(intp_num_queues, 1);
+    strArr = lsf.new_stringArray(1);
+    #print lsf.intp_value(intp_num_queues);
+    lsf.stringArray_setitem(strArr, 0, queue_name);
+    #print lsf.stringArray_getitem(strArr, 0);
+    queueInfo = lsf.lsb_queueinfo(strArr,intp_num_queues,None,None,0);
+    if queueInfo != None:
+        print 'queueInfo is not null';
+    else:
+        print 'queueInfo is null'
+        return -1;
+
+    print 'queue name = %s' % queueInfo.queue;
+    print 'queue description = %s' % queueInfo.description; 
+
+    #job_id = lsf.lsb_submit(submitreq, submitreply)
+    #return job_id
+    return 0;
+
+if __name__ == '__main__':
+    print("LSF Clustername is :", lsf.ls_getclustername())
+    print(query_queue("normal"))
