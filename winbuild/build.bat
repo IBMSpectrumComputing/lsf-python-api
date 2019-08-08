f1a8738ff49bcd2a1231fa0cf0c35932d46e8332
jxgao <jxgao@cn.ibm.com>
2019-08-08 10:30:27 +0800

    update

--- winbuild/build.bat
+++ winbuild/build.bat
@@ -1,10 +1,7 @@
-del /F /S /Q build
 swig -DWIN32 -DWin64 -I%LSF_INCLUDE% -I%LSF_INCLUDE%\lsf -python ..\pythonlsf\lsf.i
 msbuild lsf.sln -property:Configuration=Release
-mkdir build
-cp X64\Release\_lsf.pyd build\
-cp X64\Release\_lsf.lib build\
-cp X64\Release\_lsf.exp build\
-cp ..\pythonlsf\lsf.py build\
-del /F /S /Q x64
+cp X64\Release\_lsf.pyd ..\pythonlsf\
+cp X64\Release\_lsf.lib ..\pythonlsf\
+cp X64\Release\_lsf.exp ..\pythonlsf\
+
 
